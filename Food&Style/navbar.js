a099696741cf6d651a270057e551f87561b523a4
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-11-13 18:07:42 +0900

    navbar menu list click event

--- Food&Style/navbar.js
+++ Food&Style/navbar.js
@@ -1,20 +1,25 @@
 const navbar = document.querySelector(".js-navbar");
 const menu = navbar.querySelector(".navbar__menu");
+const menuLists = menu.querySelectorAll(".menu__list");
 const toggleBtn = navbar.querySelector(".navbar__toggleBtn");
+const icon = toggleBtn.querySelector("i");
 
 function loadMenu() {
-    const icon = toggleBtn.querySelector("i");
-    menu.classList.toggle("active");
-    toggleBtn.classList.toggle("active");
-    if (navbar.querySelector(".active")) {
-        navbar.style.width = 100 + "%";
-        navbar.style.height = 100 + "%";
-        icon.className = "fas fa-times";
-    } else {
-        navbar.style.height = 0 + "%";
-        icon.className = "fas fa-bars";
+    if (navbar.offsetWidth <= 768) {
+        menu.classList.toggle("active");
+        toggleBtn.classList.toggle("active");
+        if (navbar.querySelector(".active")) {
+            navbar.style.width = 100 + "%";
+            navbar.style.height = 100 + "%";
+            icon.className = "fas fa-times";
+        } else {
+            navbar.style.height = 0 + "%";
+            icon.className = "fas fa-bars";
+        }
     }
 }
 
-
-toggleBtn.addEventListener("click", loadMenu)
+toggleBtn.addEventListener("click", loadMenu);
+for (const li of menuLists) {
+    li.addEventListener('click', loadMenu)
+}
